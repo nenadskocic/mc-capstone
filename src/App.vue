640975df9342df11d154b2640a2d9d9565675939
Nenad Skocic <nenad.skocic@gmail.com>
2021-09-19 22:50:29 -0400

    Added bootstrap-vue imports

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script>
+import Vue from "vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
 export default {
   name: "App",
   components: {},
