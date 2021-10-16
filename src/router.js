7c9315574294805ff047701efd90082b2876687b
Nenad Skocic <nenad.skocic@gmail.com>
2021-10-16 11:54:38 -0400

    Added Login component to home and login path.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import vue from 'vue'
 import Router from 'vue-router'
+import Login from './components/Login.vue'
 
 export default new Router({
     mode: "history",
     routes: [
-
+        { path: '/', name: 'home', component: Login },
+        { path: '/login', name: 'login', component: Login },
     ]
 })
 
