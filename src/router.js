6d35539a71fd892ffae5a2bea7c97b30e09062a5
Nenad Skocic <nenad.skocic@gmail.com>
2021-10-16 13:21:49 -0400

    Component SignUp added to router path.

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
 
 export default new Router({
     mode: "history",
     routes: [
         { path: '/', name: 'home', component: Login },
         { path: '/login', name: 'login', component: Login },
+        { path: '/signup', name: 'signup', component: SignUp },
     ]
 })
 
