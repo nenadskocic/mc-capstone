1a726dcc2faf95ba534fdad1e2e82e836753d312
Nenad Skocic <nenad.skocic@gmail.com>
2021-10-17 07:34:29 -0400

    Fixed positioning and styling on Login page.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="loginPage">
     <div class="jumbotron text-center">
       <b-row>
-        <h4 id="signIn">Sign In</h4>
+        <h4>Sign In</h4>
       </b-row>
       <b-row>
         <b-col sm="3">
@@ -20,14 +20,14 @@
           <b-input type="password" class="form-control form-control-md" />
         </b-col>
       </b-row>
-      <b-row id="submitBtn">
+      <b-row id="submit">
         <b-button type="submit" class="btn btn-dark btn-lg btn-block">
           Send
         </b-button>
       </b-row>
       <b-row>
         <router-link to="/signup" id="signUpLink"
-          >Don't have account? Click here.</router-link
+          ><p>Forgot your password?</p></router-link
         >
       </b-row>
     </div>
@@ -38,9 +38,9 @@
 </script>
 
 <style>
-.login {
-  position: relative;
-  height: 100vh;
+.loginPage {
+  background-color: lightgray;
+  height: 90vh;
 }
 .jumbotron {
   position: absolute;
@@ -48,6 +48,41 @@
   left: 50%;
   transform: translate(-50%, -50%);
   width: 600px;
-  background-color: gray;
+  background-color: white;
+  border-style: solid;
+  border-color: black;
+  background-color: whitesmoke;
+}
+h4 {
+  align-items: center;
+  font-size: 26px;
+  color: black;
+  padding-top: 20px;
+}
+.row {
+  align-items: center;
+  padding: 2.5px;
+}
+.col-sm-3 label {
+  float: right;
+}
+
+#submit {
+  margin-left: 155px;
+  padding-top: 20px;
+  width: 20%;
+}
+
+#submit button {
+  background-color: orange;
+  color: black;
+}
+.row #signUpLink {
+  color: black !important;
+  padding-top: 50px;
+}
+.row #signUpLink p {
+  float: right;
+  padding-right: 10px;
 }
 </style>
