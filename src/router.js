53eaea7528eb7ab901389cc30bb728c7782bd74b
Nenad Skocic <nenad.skocic@gmail.com>
2021-11-27 15:29:45 -0500

    Router path re-ordering.

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import vue from 'vue'
 import Router from 'vue-router'
-import Login from './components/Login.vue'
-import SecurityQuestions from './components/SecurityQuestions.vue'
-import PasswordChange from './components/PasswordChange.vue'
+import Login from './components/public/Login.vue'
+import SecurityQuestions from './components/public/SecurityQuestions.vue'
+import PasswordChange from './components/public/PasswordChange.vue'
 import UserProfiles from './components/admin/UserProfiles.vue'
 import About from './components/public/About.vue'
 import Services from './components/public/Services.vue'
@@ -13,21 +13,19 @@ import Contact from './components/public/Contact.vue'
 export default new Router({
     mode: "history",
     routes: [
-        // User paths
+        // Public paths
         { path: '/', name: 'home', component: Login },
-        { path: '/login', name: 'login', component: Login },
-        { path: '/securityQuestions', name: 'securityQuestions', component: SecurityQuestions },
-        { path: '/passwordChange', name: 'passwordChange', component: PasswordChange },
+        { path: '/public/login', name: 'login', component: Login },
+        { path: '/public/securityQuestions', name: 'securityQuestions', component: SecurityQuestions },
+        { path: '/public/passwordChange', name: 'passwordChange', component: PasswordChange },
+        { path: '/public/about', name: 'about', component: About },
+        { path: '/public/services', name: 'services', component: Services },
+        { path: '/public/news', name: 'news', component: News },
+        { path: '/public/team', name: 'team', component: Team },
+        { path: '/public/contact', name: 'contact', component: Contact },
 
         // Admin paths
-        { path: '/admin/userProfiles', name: 'userProfiles', component: UserProfiles },
-
-        // Public paths
-        { path: '/about', name: 'about', component: About },
-        { path: '/services', name: 'services', component: Services },
-        { path: '/news', name: 'news', component: News },
-        { path: '/team', name: 'team', component: Team },
-        { path: '/contact', name: 'contact', component: Contact },
+        { path: '/admin/userProfiles', name: 'userProfiles', component: UserProfiles, meta: { requiresAuth: true } },
     ]
 })
 
