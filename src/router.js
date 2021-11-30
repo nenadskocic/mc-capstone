761172446735337dabdee4eeb06d433792aa781e
Nenad Skocic <nenad.skocic@gmail.com>
2021-11-30 18:57:55 -0500

    Added driver & truck paths to router.

--- src/router.js
+++ src/router.js
@@ -4,12 +4,15 @@ import Login from './components/public/Login.vue'
 import Registration from './components/public/Registration.vue'
 import SecurityQuestions from './components/public/SecurityQuestions.vue'
 import PasswordChange from './components/public/PasswordChange.vue'
-import UserProfiles from './components/admin/UserProfiles.vue'
 import About from './components/public/About.vue'
 import Services from './components/public/Services.vue'
 import News from './components/public/News.vue'
 import Team from './components/public/Team.vue'
 import Contact from './components/public/Contact.vue'
+
+import UserProfiles from './components/admin/UserProfiles.vue'
+import DriverProfiles from './components/admin/DriverProfiles.vue'
+import TruckProfiles from './components/admin/TruckProfiles.vue'
 
 export default new Router({
     mode: "history",
@@ -27,7 +30,9 @@ export default new Router({
         { path: '/public/contact', name: 'contact', component: Contact },
 
         // Admin paths
-        { path: '/admin/userProfiles', name: 'userProfiles', component: UserProfiles, meta: { requiresAuth: true } },
+        { path: '/admin/profiles/user', name: 'userProfiles', component: UserProfiles, meta: { requiresAuth: true } },
+        { path: '/admin/profiles/driver', name: 'driverProfiles', component: DriverProfiles, meta: { requiresAuth: true } },
+        { path: '/admin/profiles/truck', name: 'truckProfiles', component: TruckProfiles, meta: { requiresAuth: true } },
     ]
 })
 
