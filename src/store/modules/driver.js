a903bb908b5b59578f13b1542d66fe64071ad7ec
Nenad Skocic <nenad.skocic@gmail.com>
2021-11-30 19:01:58 -0500

    Name change.

---
+++ src/store/modules/driver.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const resource_uri = "http://localhost:3000/driver";
+
+export default {
+    state: {
+        drivers: []
+    },
+    getters: {
+        allDrivers: (state) => state.drivers
+    },
+    actions: {
+        async fetchDrivers({ commit }) {
+            const res = await axios.get(resource_uri);
+            commit('SET_DRIVERS', res.data)
+        },
+        async addDriver({ commit }, driver) {
+            const res = await axios.post(resource_uri, driver);
+            commit('ADD_DRIVER', res.data);
+        },
+        async updateDriver({ commit }, driver) {
+            const res = await axios.put(`${resource_uri}/${driver.user_id}`, driver);
+            commit('UPDATE_DRIVER', res.data);
+        },
+        async deleteDriver({ commit }, driver) {
+            await axios.delete(`${resource_uri}/${driver.user_id}`);
+            commit('DELETE_DRIVER', driver.user_id);
+        },
+    },
+    mutations: {
+        SET_DRIVERS: (state, drivers) => (state.drivers = drivers),
+        ADD_DRIVER: (state, driver) => {
+            state.drivers.push(driver);
+        },
+        UPDATE_DRIVER: (state, editedDriver) => {
+            const index = state.drivers.findIndex(u => u.user_id === editedDriver.user_id);
+            
+            if(index !== -1) {1
+                state.drivers.splice(index, 1, editedDriver);
+            }    
+        },
+        DELETE_DRIVER: (state, user_id) => {
+            let drivers = state.drivers.filter(u => u.user_id != user_id)
+            state.drivers = drivers;
+        }
+
+    }
+}
