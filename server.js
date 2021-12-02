b9a3f230f34b74436230f3d0b6daf501c75af967
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-01 23:00:26 -0500

    Trailer GET, PUT, POST & DELETE API complete.

--- server.js
+++ server.js
@@ -36,15 +36,19 @@ async function startup()
   await db.run("DROP TABLE IF EXISTS user_address");  
   await db.run("DROP TABLE IF EXISTS user_auth");
   await db.run("DROP TABLE IF EXISTS truck");
+  await db.run("DROP TABLE IF EXISTS trailer");
  
   await db.run("CREATE TABLE user (user_id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, user_email TEXT NOT NULL UNIQUE, sin_number INTEGER NOT NULL UNIQUE, license_number TEXT NOT NULL UNIQUE, home_phone INTEGER NOT NULL UNIQUE, cell_phone INTEGER NOT NULL UNIQUE, status TEXT NOT NULL)");
   await db.run("CREATE TABLE user_address (address_id INTEGER PRIMARY KEY, street_number INTEGER NOT NULL, street_name TEXT NOT NULL, city TEXT NOT NULL, province TEXT NOT NULL, postal_code TEXT NOT NULL, country TEXT NOT NULL, user_id INTEGER NOT NULL, FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE)");
   await db.run("CREATE TABLE user_auth (auth_id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, hash_pwd TEXT NOT NULL, type TEXT NOT NULL)");
   await db.run("CREATE TABLE truck (truck_id INTEGER PRIMARY KEY, truck_no INTEGER NOT NULL UNIQUE, truck_make TEXT NOT NULL, truck_model TEXT NOT NULL, truck_year INTEGER NOT NULL, truck_plate_no TEXT NOT NULL UNIQUE, truck_type TEXT NOT NULL, truck_status TEXT NOT NULL, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE)");
+  await db.run("CREATE TABLE trailer (trailer_id INTEGER PRIMARY KEY, trailer_no INTEGER NOT NULL UNIQUE, trailer_make TEXT NOT NULL, trailer_year INTEGER NOT NULL, trailer_type TEXT NOT NULL, trailer_plate_no TEXT NOT NULL UNIQUE, trailer_length INTEGER NOT NULL, trailer_volume INTEGER NOT NULL, ride_type TEXT NOT NULL, trailer_status TEXT NOT NULL, truck_id INTEGER, FOREIGN KEY (truck_id) REFERENCES truck (truck_id) ON DELETE CASCADE)");
 
   await db.run("INSERT INTO user (user_id, first_name, last_name, user_email, sin_number, license_number, home_phone, cell_phone, status) VALUES (null, 'Nenad', 'Skocic', 'nenad@jsdtransportation.ca', 111111, 'S4913-47535-73581', 9055782526, 3654764643, 'Active')");
   await db.run("INSERT INTO user_address (street_number, street_name, city, province, postal_code, country, user_id) VALUES (78, 'Sunrise Dr', 'Hamilton', 'ON', 'L8K 4C3', 'Canada', 1)");
   await db.run("INSERT INTO truck (truck_id, truck_no, truck_make, truck_model, truck_year, truck_plate_no, truck_type, truck_status, user_id) VALUES (null, 101, 'Volvo', 'T680', 2017, '409-4PL', 'Semi-trailer', 'Active', 1)");
+  await db.run("INSERT INTO trailer (trailer_id, trailer_no, trailer_make, trailer_year, trailer_type, trailer_plate_no, trailer_length, trailer_volume, ride_type, trailer_status, truck_id) VALUES (null, 37995, 'Great Dane', 2015, 'Reefer', 'M6540C', 50,  3001, 'Air', 'Active', 1)");
+
   //await db.run("INSERT INTO user (user_id, first_name, last_name, user_email, sin_number, license_number, home_phone, cell_phone, status) VALUES (null, 'T', 'T', 'T@jsdtransportation.ca', 343434, 'S4913-47535-73781', 9055784526, 3658764643, 'Active')");
   //await db.run("INSERT INTO user_address (street_number, street_name, city, province, postal_code, country, user_id) VALUES (78, 'Sunrise Dr', 'Hamilton', 'ON', 'L8K 4C3', 'Canada', 2)");
   register_user();
@@ -64,7 +68,10 @@ async function register_user() {
   }
 }
 
-// API - User Authentication
+/**
+ * API - User Authentication
+ * 
+ */
 app.get("/auth", async function(req, res) {
     const data = await db.all("SELECT auth_id as id, * FROM user_auth");
     res.send(data);
@@ -94,7 +101,10 @@ app.delete("/auth/:id", async function(req, res)
   res.send(stmt);  
 });
 
-// API - Driver_Info (JOIN)
+/**
+ * API - Driver Info (JOIN)
+ * 
+ */
 app.get("/driver", async function(req, res) {
   const data = await db.all("SELECT * FROM user INNER JOIN user_address ON user.user_id = user_address.user_id");
   res.send(data);
@@ -132,7 +142,10 @@ app.put("/driver/:id", async function(req, res)
   res.send(stmt);
 });
 
-// API - Truck
+/**
+ * API - Truck
+ * 
+ */
 app.get("/truck", async function(req, res) {
   const data = await db.all("SELECT truck_id as id, * FROM truck");
   res.send(data);
@@ -154,16 +167,12 @@ app.delete("/truck/:id", async function(req, res)
 app.put("/truck/:id", async function(req, res)
 {
   const truck_id = req.params.id;
-  const truck_no = req.body.truck_no;
   const truck_make = req.body.truck_make;
   const truck_model= req.body.truck_model;
   const truck_year = req.body.truck_year;
   const truck_plate_no = req.body.truck_plate_no;
   const truck_type = req.body.truck_type;
   const truck_status = req.body.truck_status;
-  const user_id = req.body.user_id;
-
-  console.log(truck_id, truck_no, truck_make, truck_model, truck_year, truck_plate_no, truck_type, truck_status, user_id );
 
   const stmt = await db.prepare("UPDATE truck SET truck_make=?, truck_model=?, truck_year=?, truck_plate_no=?, truck_type=?, truck_status=? WHERE truck_id=?");
   stmt.run(truck_make, truck_model, truck_year, truck_plate_no, truck_type, truck_status, truck_id);
@@ -188,7 +197,68 @@ app.post("/truck", async function(req, res)
     const stmt = await db.prepare("INSERT INTO truck VALUES (?,?,?,?,?,?,?,?,?)");
     stmt.run(null, truck_no, truck_make, truck_model, truck_year, truck_plate_no, truck_type, truck_status, null);
     stmt.finalize();
-    res.json( { truck: { truck_no: truck_no, truck_make: truck_make, truck_model: truck_model, truck_year: truck_year, truck_plate_no: truck_plate_no, truck_type: truck_type, truck_status: truck_status } });
+    res.json( { truck: { truck_no: truck_no, truck_make: truck_make, truck_model: truck_model, truck_year: truck_year, truck_plate_no: truck_plate_no, truck_type: truck_type, truck_status: truck_status }});
+  }
+});
+
+/**
+ * API - Trailer
+ * 
+ */
+app.get("/trailer", async function(req, res) {
+  const data = await db.all("SELECT trailer_id as id, * FROM trailer");
+  res.send(data);
+});
+app.get("/trailer/:id", async function(req, res) {
+  const trailer_id = req.params.id;
+  const stmt = "SELECT * FROM trailer WHERE trailer_id=?";
+  const data = await db.all(stmt, [trailer_id]);
+  res.send(data);
+});
+app.delete("/trailer/:id", async function(req, res)
+{
+  const trailer_id = req.params.id;
+  const stmt = await db.prepare("DELETE FROM trailer WHERE trailer_id=?");
+  stmt.run(trailer_id);
+  stmt.finalize();
+  res.send(stmt);  
+});
+app.put("/trailer/:id", async function(req, res)
+{
+  const trailer_id = req.params.id;
+  const trailer_make = req.body.trailer_make;
+  const trailer_year= req.body.trailer_year;
+  const trailer_length = req.body.trailer_length;
+  const trailer_volume = req.body.trailer_volume;
+  const ride_type = req.body.ride_type;
+  const trailer_status = req.body.trailer_status
+
+  const stmt = await db.prepare("UPDATE trailer SET trailer_make=?, trailer_year=?, trailer_length=?, trailer_volume=?, ride_type=?, trailer_status=? WHERE trailer_id=?");
+  stmt.run(trailer_make, trailer_year, trailer_length, trailer_volume, ride_type, trailer_status, trailer_id);
+  stmt.finalize();
+  res.send(stmt);
+});
+app.post("/trailer", async function(req, res)
+{
+  const trailer_no = req.body.trailer_no;
+  const trailer_make = req.body.trailer_make;
+  const trailer_year = req.body.trailer_year;
+  const trailer_type = req.body.trailer_type;
+  const trailer_plate_no = req.body.trailer_plate_no;
+  const trailer_length = req.body.trailer_length;
+  const trailer_volume = req.body.trailer_volume;
+  const ride_type = req.body.ride_type;
+  const trailer_status = req.body.trailer_status;
+
+  const trailer = await db.all("SELECT * FROM trailer WHERE trailer_no = '" + trailer_no + "' OR trailer_plate_no = '" + trailer_plate_no + "'");
+
+  if(trailer.length > 0) {
+    res.status(200).json("Duplicate");
+  } else {
+    const stmt = await db.prepare("INSERT INTO trailer VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+    stmt.run(null, trailer_no, trailer_make, trailer_year, trailer_type, trailer_plate_no, trailer_length, trailer_volume, ride_type, trailer_status, null);
+    stmt.finalize();
+    res.json( { trailer: { trailer_no: trailer_no, trailer_make: trailer_make, trailer_year: trailer_year, trailer_type: trailer_type, trailer_plate_no: trailer_plate_no, trailer_length: trailer_length, trailer_volume: trailer_volume, ride_type: ride_type, trailer_status: trailer_status }});
   }
 });
 
