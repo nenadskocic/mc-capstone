de2c1542b78f9b2374e1ef03580872b73242fb7a
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-01 23:03:56 -0500

    trailer module imported to store.

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import auth from './modules/auth';
 import driver from './modules/driver';
 import truck from './modules/truck';
+import trailer from './modules/trailer';
 
 Vue.use(Vuex);
 
@@ -11,5 +12,6 @@ export default new Vuex.Store({
         auth,
         driver,
         truck,
+        trailer,
     }
 });
