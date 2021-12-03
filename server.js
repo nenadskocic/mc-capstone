293ca431654525d9d909ea8295a40dae6b4e779c
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-03 14:49:06 -0500

    User & User_Address GET, PUT, POST & DELETE API complete.

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+/**
+ * I, Nenad Skocic, 000107650 certify that this material is my original work. 
+ * No other person's work has been used without due acknowledgment.
+ */
 const sqlite3 = require('sqlite3').verbose();
 const sqlite = require('sqlite');
 const express = require('express');
@@ -5,13 +9,15 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('./config');
-const { BFormInvalidFeedback } = require('bootstrap-vue');
 const app = express();
 const router = express.Router();
 
 const admin_username = "admin.admin";
 const pass = "admin";
 const saltRounds = 10;
+
+const user_username = "user.user";
+const pass2 = "user";
 
 var corsOptions = {
   origin: "http://localhost:8080",
@@ -47,20 +53,31 @@ async function startup()
   await db.run("DROP TABLE IF EXISTS user_auth");
   await db.run("DROP TABLE IF EXISTS truck");
   await db.run("DROP TABLE IF EXISTS trailer");
- 
+  await db.run("DROP TABLE IF EXISTS client");
+  await db.run("DROP TABLE IF EXISTS client_address");
+
   await db.run("CREATE TABLE user (user_id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, user_email TEXT NOT NULL UNIQUE, sin_number INTEGER NOT NULL UNIQUE, license_number TEXT NOT NULL UNIQUE, home_phone INTEGER UNIQUE, cell_phone INTEGER UNIQUE, status TEXT NOT NULL)");
   await db.run("CREATE TABLE user_address (address_id INTEGER PRIMARY KEY, street_number INTEGER NOT NULL, street_name TEXT NOT NULL, city TEXT NOT NULL, province TEXT NOT NULL, postal_code TEXT NOT NULL, country TEXT NOT NULL, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE)");
   await db.run("CREATE TABLE user_auth (auth_id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, hash_pwd TEXT NOT NULL, is_admin INTEGER NOT NULL)");
   await db.run("CREATE TABLE truck (truck_id INTEGER PRIMARY KEY, truck_no INTEGER NOT NULL UNIQUE, truck_make TEXT NOT NULL, truck_model TEXT NOT NULL, truck_year INTEGER NOT NULL, truck_plate_no TEXT NOT NULL UNIQUE, truck_type TEXT NOT NULL, truck_status TEXT NOT NULL, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE)");
   await db.run("CREATE TABLE trailer (trailer_id INTEGER PRIMARY KEY, trailer_no INTEGER NOT NULL UNIQUE, trailer_make TEXT NOT NULL, trailer_year INTEGER NOT NULL, trailer_type TEXT NOT NULL, trailer_plate_no TEXT NOT NULL UNIQUE, trailer_length INTEGER NOT NULL, trailer_volume INTEGER NOT NULL, ride_type TEXT NOT NULL, trailer_status TEXT NOT NULL, truck_id INTEGER, FOREIGN KEY (truck_id) REFERENCES truck (truck_id) ON DELETE CASCADE)");
+  await db.run("CREATE TABLE client (client_id INTEGER PRIMARY KEY, client_name TEXT NOT NULL UNIQUE, client_phone_no INTEGER NOT NULL UNIQUE, client_type TEXT NOT NULL, client_status TEXT NOT NULL)");
+  await db.run("CREATE TABLE client_address (client_address_id INTEGER PRIMARY KEY, street_number INTEGER NOT NULL, street_name TEXT NOT NULL, city TEXT NOT NULL, province TEXT NOT NULL, postal_code TEXT NOT NULL, country TEXT NOT NULL, client_id INTEGER, FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE)");
+
 
   await db.run("INSERT INTO user (user_id, first_name, last_name, user_email, sin_number, license_number, home_phone, cell_phone, status) VALUES (null, 'Nenad', 'Skocic', 'nenad@jsdtransportation.ca', 111111, 'S4913-47535-73581', 9055782526, 3654764643, 'Active')");
   await db.run("INSERT INTO user_address (street_number, street_name, city, province, postal_code, country, user_id) VALUES (78, 'Sunrise Dr', 'Hamilton', 'ON', 'L8K 4C3', 'Canada', 1)");
+  // await db.run("INSERT INTO user (user_id, first_name, last_name, user_email, sin_number, license_number, home_phone, cell_phone, status) VALUES (null, 'Sinisa', 'Radisa', 'sinisa@jsdtransportation.ca', 343434, 'S4813-47535-65781', 9055784526, 365856864, 'Active')");
+  // await db.run("INSERT INTO user_address (street_number, street_name, city, province, postal_code, country, user_id) VALUES (48, 'Winslow Way', 'Hamilton', 'ON', 'L8K 5R9', 'Canada', 2)");
+
   await db.run("INSERT INTO truck (truck_id, truck_no, truck_make, truck_model, truck_year, truck_plate_no, truck_type, truck_status, user_id) VALUES (null, 101, 'Volvo', 'T680', 2017, '409-4PL', 'Semi-trailer', 'Active', 1)");
+
   await db.run("INSERT INTO trailer (trailer_id, trailer_no, trailer_make, trailer_year, trailer_type, trailer_plate_no, trailer_length, trailer_volume, ride_type, trailer_status, truck_id) VALUES (null, 37995, 'Great Dane', 2015, 'Reefer', 'M6540C', 50,  3001, 'Air', 'Active', 1)");
 
-  //await db.run("INSERT INTO user (user_id, first_name, last_name, user_email, sin_number, license_number, home_phone, cell_phone, status) VALUES (null, 'T', 'T', 'T@jsdtransportation.ca', 343434, 'S4913-47535-73781', 9055784526, 3658764643, 'Active')");
-  //await db.run("INSERT INTO user_address (street_number, street_name, city, province, postal_code, country, user_id) VALUES (78, 'Sunrise Dr', 'Hamilton', 'ON', 'L8K 4C3', 'Canada', 2)");
+  await db.run("INSERT INTO client (client_id, client_name, client_phone_no, client_type, client_status) VALUES (null, 'ABD GOUP', 9567171995, 'Importer', 'Active')");
+  await db.run("INSERT INTO client_address (client_address_id, street_number, street_name, city, province, postal_code, country, client_id) VALUES (null, 12122, 'Jeff Drive', 'Laredo', 'TX', '78045', 'USA', 1)");
+  await db.run("INSERT INTO client (client_id, client_name, client_phone_no, client_type, client_status) VALUES (null, 'Fluke', 9055780677, 'Exporter', 'Active')");
+  await db.run("INSERT INTO client_address (client_address_id, street_number, street_name, city, province, postal_code, country, client_id) VALUES (null, 450, 'Sherman Ave N', 'Hamilton', 'ON', 'L8L 8J6', 'Canada', 2)");
   register_user();
 
   const server = app.listen(3000, function(){
@@ -72,7 +89,9 @@ startup();
 async function register_user() {
   try {
     const hash = await bcrypt.hash(pass, saltRounds);
+    const hash2 = await bcrypt.hash(pass2, saltRounds);
     await db.run("INSERT INTO user_auth (auth_id, username, hash_pwd, is_admin) VALUES (?, '" + admin_username + "', '" + hash + "', '1')");
+    await db.run("INSERT INTO user_auth (auth_id, username, hash_pwd, is_admin) VALUES (?, '" + user_username + "', '" + hash2 + "', '0')");
   } catch(e) {
     console.log(e)
   }
@@ -138,7 +157,7 @@ app.post("/driver", async function(req, res)
   const status = req.body.status;
 
   const driver = await db.all("SELECT * FROM user WHERE user_email = '" + user_email + "' OR home_phone = '" + home_phone + "' OR cell_phone = '" + cell_phone + "' OR sin_number = '" + sin_number + "' OR license_number = '" + license_number + "'");
-  console.log(driver);
+
   if(driver.length > 0) {
     res.status(200).json("Duplicate");
   } else {
@@ -357,6 +376,105 @@ app.post("/trailer", async function(req, res)
     res.json( { trailer: { trailer_no: trailer_no, trailer_make: trailer_make, trailer_year: trailer_year, trailer_type: trailer_type, trailer_plate_no: trailer_plate_no, trailer_length: trailer_length, trailer_volume: trailer_volume, ride_type: ride_type, trailer_status: trailer_status }});
   }
 });
+
+/**
+ * API - Client
+ * 
+ */
+app.get("/client", async function(req, res) {
+  const data = await db.all("SELECT client_id as id, * FROM client");
+  res.send(data);
+});
+app.post("/client", async function(req, res)
+{
+  const client_name = req.body.client_name;
+  const client_phone_no = req.body.client_phone_no;
+  const client_type = req.body.client_type;
+  const client_status = req.body.client_status;
+
+
+  const client = await db.all("SELECT * FROM client WHERE client_name = '" + client_name + "' OR client_phone_no = '" + client_phone_no + "'");
+
+  if(client.length > 0) {
+    res.status(200).json("Duplicate");
+  } else {
+    const stmt = await db.prepare("INSERT INTO client VALUES (?,?,?,?,?)");
+    stmt.run(null, client_name, client_phone_no, client_type, client_status);
+    stmt.finalize();
+    res.json( { client: { client_name: client_name, client_phone_no: client_phone_no, client_type: client_type, client_status: client_status }});
+  }
+});
+app.put("/client/:id", async function(req, res)
+{
+  const client_id = req.params.id;
+  const client_phone_no = req.body[0].client_phone_no;
+  const client_type = req.body[0].client_type;
+  const client_status = req.body[0].client_status;
+
+  const stmt = await db.prepare("UPDATE client SET client_phone_no=?, client_type=?, client_status=? WHERE client_id=?");
+  stmt.run( client_phone_no, client_type, client_status, client_id);
+  stmt.finalize();
+  res.send(stmt);
+});
+
+/**
+ * API - Client_Address
+ * 
+ */
+ app.get("/client_address", async function(req, res) {
+  const data = await db.all("SELECT client_address_id as id, * FROM client_address");
+  res.send(data);
+});
+app.post("/client_address", async function(req, res)
+{
+  const street_number = req.body.street_number;
+  const street_name = req.body.street_name;
+  const city = req.body.city;
+  const province = req.body.province;
+  const postal_code = req.body.postal_code;
+  const country = req.body.country;
+
+  const last_insert = await db.all("SELECT last_insert_rowid();");
+  const client_id = last_insert[0]['last_insert_rowid()'];
+
+  const stmt = await db.prepare("INSERT INTO client_address VALUES (?,?,?,?,?,?,?,?)");
+  stmt.run(null, street_number, street_name, city, province, postal_code, country, client_id);
+  stmt.finalize();
+  res.json( { client_address: { street_number: street_number, street_name: street_name, city: city, province: province, postal_code: postal_code, country: country, client_id: client_id}}); 
+});
+app.put("/client_address/:id", async function(req, res)
+{
+  const client_address_id = req.params.id;
+  const street_number = req.body[0].street_number;
+  const street_name = req.body[0].street_name;
+  const city = req.body[0].city;
+  const province = req.body[0].province;
+  const postal_code = req.body[0].postal_code;
+  const country = req.body[0].country;
+
+  const stmt = await db.prepare("UPDATE client_address SET street_number=?, street_name=?, city=?, province=?, postal_code=?, country=? WHERE client_address_id=?");
+  stmt.run( street_number, street_name, city, province, postal_code, country, client_address_id);
+  stmt.finalize();
+  res.send(stmt);
+});
+
+/**
+ * API - Client_Info (Client & Client_Address JOIN)
+ * 
+ */
+ app.get("/client_info", async function(req, res) {
+  const data = await db.all("SELECT * FROM client INNER JOIN client_address ON client.client_id = client_address.client_id");
+  res.send(data);
+});
+app.delete("/client_info/:id", async function(req, res)
+{
+  const client_id = req.params.id;
+  const stmt = await db.prepare("DELETE FROM client WHERE client_id=?");
+  stmt.run(client_id);
+  stmt.finalize();
+  res.send(stmt);  
+});
+
 
 /**
  * Registration
