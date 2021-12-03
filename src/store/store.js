f53cb64bd989a545590a0754ef4314fc7bb0535b
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-03 14:50:07 -0500

    Added client module to store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,14 @@
+/**
+ * I, Nenad Skocic, 000107650 certify that this material is my original work. 
+ * No other person's work has been used without due acknowledgment.
+ */
 import Vue from 'vue';
 import Vuex from 'vuex';
 import user from './modules/user';
 import driver from './modules/driver';
 import truck from './modules/truck';
 import trailer from './modules/trailer';
+import client from './modules/client';
 
 Vue.use(Vuex);
 
@@ -13,5 +18,6 @@ export default new Vuex.Store({
         driver,
         truck,
         trailer,
+        client,
     }
 });
