787c54e7d81e3b30c260510baade094c12fde5af
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-03 22:18:22 -0500

    Renamed page.

--- src/components/user/Dispatch.vue
+++
@@ -1,9 +0,0 @@
-/**
- * I, Nenad Skocic, 000107650 certify that this material is my original work. 
- * No other person's work has been used without due acknowledgment.
- */
-<template>
-  <div>
-    <h1>Dispatch Page</h1>
-  </div>
-</template>
